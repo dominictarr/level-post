70fa99354f8f62a2dae3e980f85a72769a3bbf09
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-17 11:36:15 +0100

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# level-post
+
+
+[![travis](https://travis-ci.org/dominictarr/level-post.png?branch=master)
+](https://travis-ci.org/dominictarr/level-post)
+
+[![testling](http://ci.testling.com/dominictarr/level-post.png)
+](http://ci.testling.com/dominictarr/level-post)
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,39 @@
+
+var sr = require('string-range')
+
+module.exports = function post (db, opts, each) {
+  if(!each)
+    each = opts, opts = {}
+
+  if('function' === db.post)
+    return db.post(opts, each)
+
+  var checker = sr.checker(opts)
+
+  function onPut (key, val) {
+    if(checker(key))
+      each({type: 'put', key: key, value: val})
+  }
+
+  function onDel (key, val) {
+    if(checker(key))
+      each({type: 'del', key: key, value: val})
+  }
+
+  function onBatch (ary) {
+    ary.forEach(function (op) {
+      if(checker(op.key))
+        each(op)
+    })
+  }
+
+  db.on('put', onPut)
+  db.on('del', onDel)
+  db.on('batch', onBatch)
+
+  return function () {
+    db.removeListener('put', onPut)
+    db.removeListener('del', onPut)
+    db.removeListener('batch', onPut)
+  }
+}
